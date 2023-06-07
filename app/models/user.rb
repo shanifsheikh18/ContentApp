93c11781a95355d9b505c7b95b4a5e8631269c56
shanif <shanifsheikh.18@gmail.com>
2023-06-07 15:26:39 +0500

    home page and devise login css done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
- root to: "home#index"
+  root to: "home#index"
+  devise_for :users, controllers: { registrations: 'users/registrations' }
 end
